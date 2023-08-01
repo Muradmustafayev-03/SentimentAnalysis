e2abfa8f3302e93dabcd78c639f8be16a4b65299
Murad <muradmustafayev03@gmail.com>
2023-08-01 14:37:16 +0400

    Added filter_and_extract_lemma to preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -6,7 +6,7 @@ nlp = spacy.load("en_core_web_sm")
 
 def filter_tokens(text: str) -> str:
     """
-    Filters out punctuation and stop tokens.
+    Filter out punctuation and stop tokens.
     :param text: input text (sentence)
     :return: text without punctuation and stop words
     """
@@ -17,10 +17,21 @@ def filter_tokens(text: str) -> str:
 
 def extract_lemma(text: str) -> str:
     """
-    Extracts lemma(base) from each word in the text
+    Extract lemma(base) from each word in the text.
     Example: feeling -> feel; pencils -> pencil; exhausted -> exhaust
     :param text: input text (sentence)
     :return: text with each word replaced to its base word
     """
     doc = nlp(text)
     return ' '.join([token.lemma_ for token in doc])
+
+
+def filter_and_extract_lemma(text: str) -> str:
+    """
+    Filter out punctuation and stop tokens and extract lemma(base) from each word in the text.
+    :param text: input text (sentence)
+    :return: text without punctuation and stop words and each word replaced to its base word
+    """
+    doc = nlp(text)
+    filtered = [token.lemma_ for token in doc if not token.is_stop and not token.is_punct]
+    return ' '.join(filtered)
