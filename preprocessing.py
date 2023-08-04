0c487b53b7e4f4197729eccd6edecc0cde7cdbbd
Murad <muradmustafayev03@gmail.com>
2023-08-04 23:20:55 +0400

    added enumerate_encode and enumerate_decode to preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -97,3 +97,24 @@ def one_hot_decode(encoded: np.array, classes: pd.Series) -> np.array:
     """
     classes = sorted(classes.unique().tolist())
     return np.array([classes[np.argmax(one_hot)] for one_hot in encoded])
+
+
+def enumerate_encode(column: pd.Series) -> np.array:
+    """
+    Converts a data series of classes into enumerated array
+    :param column: the data Series or DataFrame column to encode
+    :return: 2D array with encoded data
+    """
+    classes = sorted(column.unique().tolist())
+    return np.array([classes.index(value) for value in column])
+
+
+def enumerate_decode(encoded: np.array, classes: pd.Series) -> np.array:
+    """
+    Decodes enumerated values according to existing classes
+    :param encoded: enumerated numpy array, either from enumerate_encode function or from the model output
+    :param classes: classes column of the source dataset
+    :return: numpy array with the name of the class corresponding to each enumerated value
+    """
+    classes = sorted(classes.unique().tolist())
+    return np.array([classes[value] for value in encoded])
